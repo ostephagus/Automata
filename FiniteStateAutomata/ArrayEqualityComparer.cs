7242c487743675edb45f13a0a0dc9a40ad1f549e
ostephagus <37499766+ostephagus@users.noreply.github.com>
2023-06-06 08:42:40 +0100

    Starting to implement IEqualityComparer

---
+++ FiniteStateAutomata/ArrayEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteStateAutomata
+{
+    public class ArrayEqualityComparer<TArray> : IEqualityComparer<TArray[]> where TArray : IComparable
+    {
+        public bool Equals(TArray[] x, TArray[] y)
+        {
+            if (x.Length != y.Length) //If the arrays are different lengths, they are definitely not the same
+            {
+                return false;
+            }
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (!x[i].Equals(y[i])) //Check equality for each element
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
